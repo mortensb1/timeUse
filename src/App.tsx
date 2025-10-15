b5c4f6344ced1e57db4e4bcf80599178af5ede4b
Morten Silkjær Bregnballe <mortensb1@gmail.com>
2025-10-15 16:05:55 +0200

    Add functions for supabase
    Get and add values to db

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 import { useState } from 'react'
-import {Button, Container, Typography} from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 import supabase from '../utils/supabase'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  async function uploadTime(amount_people: number, hours: number) {
+    const { error } = await supabase
+      .from("time")
+      .insert({ time_used: 10 })
+    if (error) {
+      console.log("ERROR:", error)
+    }
+    const { data } = await supabase
+      .from("time")
+      .select()
+    console.log(data)
+  }
+
   return (
-    <Container sx={{ textAlign: "center", mt: 5}}>
+    <Container sx={{ textAlign: "center", mt: 5 }}>
       <Typography variant="body1">Count: {count}</Typography>
       <Button
         variant="contained"
         color="primary"
-        sx={{ mt: 2}}
-        onClick={() => setCount(count + 1)}
-        >
-          Increment
-        </Button>
+        sx={{ mt: 2 }}
+        onClick={() => uploadTime(1, 1)}
+      >
+        Increment
+      </Button>
     </Container>
   )
 }
