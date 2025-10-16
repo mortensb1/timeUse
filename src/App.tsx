dfd48e24373dcc217a9b517a954ec075f916f557
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2025-10-16 18:54:50 +0200

    Works (No date Picker)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,18 @@ import * as React from "react";
 import { DesktopTimePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { Dayjs } from "dayjs";
-import { Button, Container, Typography, TextField } from "@mui/material";
+import dayjs, { Dayjs } from "dayjs";
+import { Button, 
+  Container, 
+  Typography, 
+  TextField, 
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow, 
+  Paper} from "@mui/material";
 import supabase from '../utils/supabase'
 import './App.css'
 
@@ -12,6 +22,16 @@ function App() {
   const [timeStart, setTimeStart] = React.useState<Dayjs | null>();
   const [timeEnd, setTimeEnd] = React.useState<Dayjs | null>();
   const [amountPeople, setAmountPeople] = React.useState<number>();
+
+  const [values, setValues] = React.useState<any[] | null>(null);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const result = await getValues();
+      setValues(result);
+    }
+    fetchData();
+  }, []);
 
 
   async function uploadTime() {
@@ -21,54 +41,64 @@ function App() {
     if (error) {
       console.log("ERROR:", error)
     }
+
+    var data = await getValues()
+    console.log(data)
   }
 
   async function getValues(): Promise<any[] | null> {
     const { error, data } = await supabase
       .from("time")
       .select()
+      .order("created_at", { ascending: false })
+      .limit(15);
 
     if (error) {
       console.log("Error:", error)
     }
-
     return data
   }
+
+  // const data = await getValues()
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Container sx={{ textAlign: "center", }}>
           <Typography variant="h3" sx={{ 
-            position: "fixed",
             top: 10,
             left: 0,
             width: "100%",
             textAlign: "center",
             }}>Trænings tid</Typography>
-        <Container sx={{ textAlign: "center", mt: 5 }}>
-          <DesktopTimePicker 
-            label="Start tid" 
-            ampm={false} 
-            sx={{ mt: 5, mb: 2 }} 
-            onChange={(newValue) => {
-              setTimeStart(newValue)
-            }}
-          />
-          <DesktopTimePicker 
-            label="Slut tid" 
-            ampm={false} 
-            sx={{ mb: 4 }}
-            onChange={(newValue) => {
-              setTimeEnd(newValue)
-            }}
+        <Container sx={{ textAlign: "center", mt: 1 }}>
+          <Container>
+            <DesktopTimePicker 
+              label="Start tid" 
+              ampm={false} 
+              sx={{ mt: 5, mb: 2 }} 
+              onChange={(newValue) => {
+                setTimeStart(newValue)
+              }}
             />
-            <TextField
+          </Container>
+          <Container>
+            <DesktopTimePicker 
+              label="Slut tid" 
+              ampm={false} 
+              sx={{ mb: 4 }}
+              onChange={(newValue) => {
+                setTimeEnd(newValue)
+              }}
+            />
+          </Container>
+            <Container>
+              <TextField
               label="Antal mennesker"
               type="number"
-              value={amountPeople}
               onChange={(e) => setAmountPeople(Number(e.target.value))}
               sx={{ width: 250 }}
             />
+            </Container>
           <Button
             variant="contained"
             color="primary"
@@ -77,6 +107,39 @@ function App() {
             >
             Tilføj tid
           </Button>
+
+
+          <Typography variant="h4" sx={{ mt: 3, mb: 3, textAlign: "center" }}>
+            Time Entries
+          </Typography>
+          {/*  */}
+          <TableContainer component={Paper} sx={{ borderRadius: 2, boxShadow: 3, px:0 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell><b>Id</b></TableCell>
+              <TableCell><b>Tid Start</b></TableCell>
+              <TableCell><b>Tid Slut</b></TableCell>
+              <TableCell><b>Timer</b></TableCell>
+              <TableCell><b>Antal</b></TableCell>
+              <TableCell><b>Dato</b></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {values?.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.id}</TableCell>
+                <TableCell>{dayjs(item.time_start).format("HH:mm")}</TableCell>
+                <TableCell>{dayjs(item.time_end).format("HH:mm")}</TableCell>
+                <TableCell>{item.hours_used}</TableCell>
+                <TableCell>{item.people}</TableCell>
+                <TableCell>{dayjs(item.time_end).format("MM-DD")}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+          {/*  */}
         </Container>
       </Container>
     </LocalizationProvider>
